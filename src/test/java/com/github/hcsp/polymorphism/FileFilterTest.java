669314ec0f17a1a4dbb940b417f479bd8f9495e5
Bo Zhang <bo@gradle.com>
2019-06-28 10:40:06 +0800

    Initial commit

---
+++ src/test/java/com/github/hcsp/polymorphism/FileFilterTest.java
@@ -0,0 +1,27 @@
+package com.github.hcsp.polymorphism;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FileFilterTest {
+    @Test
+    public void test() throws IOException {
+        Path testPath = Paths.get(System.getProperty("basedir")).resolve("test-root");
+        Assertions.assertEquals(
+                asSet("1.txt", "5.txt", "6.txt"),
+                new HashSet<>(FileFilter.filter(testPath, ".txt")));
+        Assertions.assertEquals(
+                asSet("2.csv", "3.csv", "4.csv", "7.csv"),
+                new HashSet<>(FileFilter.filter(testPath, ".csv")));
+    }
+
+    private Set<String> asSet(String... elements) {
+        return new HashSet<>(Arrays.asList(elements));
+    }
+}
