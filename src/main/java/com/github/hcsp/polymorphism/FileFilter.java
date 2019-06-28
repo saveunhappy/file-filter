45913ae59c550d51e403e47511980d0a79df3414
Bo Zhang <bo@gradle.com>
2019-06-28 12:24:46 +0800

    Update verifyWhitelist.sh

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -24,6 +24,5 @@ public class FileFilter {
      * @param extension 要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
-    }
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
 }
