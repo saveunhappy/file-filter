1ffd1468ff9a8f7a2342d02ccf78ca73c6354e4f
Kelvin Chen <wheelchen97@gmail.com>
2019-11-14 22:29:26 +0800

    file-filter (#63)
    file-filter (#63)

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,10 +1,16 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author wheelchen
+ */
 public class FileFilter {
     public static void main(String[] args) throws IOException {
         Path projectDir = Paths.get(System.getProperty("user.dir"));
@@ -24,5 +30,26 @@ public class FileFilter {
      * @param extension 要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) {
+        List<String> files = new ArrayList<>();
+
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(rootDirectory)) {
+            for (Path path: stream) {
+                //如果是目录
+                if (path.toFile().isDirectory()) {
+                    files.addAll(filter(path, extension));
+                }
+                //如果是文件
+                else {
+                    String fileName = String.valueOf(path.getFileName());
+                    if (fileName.endsWith(extension)) {
+                        files.add(fileName);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return files;
+    }
 }
