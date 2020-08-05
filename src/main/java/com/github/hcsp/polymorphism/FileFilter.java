22836278181bc345c25aab18afd52b854c42adb1
linweikun95 <63492316+linweikun95@users.noreply.github.com>
2020-08-06 00:08:39 +0800

    feat: file filter (#166)
    feat: file filter (#166)

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,8 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileFilter {
@@ -24,5 +25,48 @@ public class FileFilter {
      * @param extension 要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
+        List<String> filteredFileNames = new ArrayList<>();
+        FileVisitTools fileVisitTools = new FileVisitTools(filteredFileNames, extension);
+        Files.walkFileTree(rootDirectory, fileVisitTools);
+        return fileVisitTools.filteredFileNames();
+    }
+
+    static class FileVisitTools implements FileVisitor<Path> {
+        private List<String> list;
+        private String string;
+
+        FileVisitTools(List<String> list, String str) {
+            this.list = list;
+            this.string = str;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path path, BasicFileAttributes basicFileAttributes) throws IOException {
+            String fileString = path.toAbsolutePath().toString();
+            if (fileString.endsWith(string)) {
+                list.add(path.getFileName().toString());
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path path, IOException e) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path path, IOException e) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        private List<String> filteredFileNames() {
+            return this.list;
+        }
+    }
 }
