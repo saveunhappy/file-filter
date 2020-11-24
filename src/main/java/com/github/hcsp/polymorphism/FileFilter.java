b89ffc22fe9fce61d7cb135140514c4abce45a98
qpWei <42760426+qipengwei@users.noreply.github.com>
2020-11-24 23:19:17 +0800

    文件过滤器 (#181)
    文件过滤器 (#181)

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,8 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileFilter {
@@ -24,5 +25,23 @@ public class FileFilter {
      * @param extension 要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
+            List<String> names = new ArrayList<>( );
+
+            //walkFileTree 方法对文件夹进行遍历IO流操作
+            Files.walkFileTree( rootDirectory, new SimpleFileVisitor<Path>() {
+                //visitFile 方法获取路径中的文件夹 并遍历查找
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    //把文件后缀相同的文件路径放到集合中
+                    if(file.getFileName().toString().endsWith( extension )) {
+                        names.add(file.getFileName().toString());
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+
+            } );
+
+            return names;
+    }
 }
