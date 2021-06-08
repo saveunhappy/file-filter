645f04b65f0156987abb6ca5bde0637a7e1fdb2c
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-06-08 21:51:59 +0800

    Feature/task (#197)
    Feature/task (#197)

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,8 +1,13 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.FileVisitResult;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileFilter {
@@ -21,8 +26,23 @@ public class FileFilter {
      * 实现一个按照扩展名过滤文件的功能
      *
      * @param rootDirectory 要过滤的文件夹
-     * @param extension 要过滤的文件扩展名，例如 .txt
+     * @param extension     要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
+        List<String> result = new ArrayList<>();
+        System.out.println(rootDirectory);
+        System.out.println(extension);
+        Files.walkFileTree(rootDirectory, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+                String fileName = file.getFileName().toString();
+                if (fileName.endsWith(extension)) {
+                    result.add(fileName);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return result;
+    }
 }
