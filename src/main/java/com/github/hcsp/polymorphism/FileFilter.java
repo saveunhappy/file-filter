0dc579c61641b1d9b7a7b7f901a6809e2d1e127c
maomao <47076197+zdmhahaha@users.noreply.github.com>
2021-10-12 23:17:55 +0800

    实现一个自定义的文件过滤器 (#208)
    实现一个自定义的文件过滤器 (#208)

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,8 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileFilter {
@@ -13,7 +14,7 @@ public class FileFilter {
             throw new IllegalStateException(testRootDir.toAbsolutePath().toString() + "不存在！");
         }
 
-        List<String> filteredFileNames = filter(testRootDir, ".csv");
+        List<String> filteredFileNames = filter(testRootDir, ".csv");  //过滤后的列表
         System.out.println(filteredFileNames);
     }
 
@@ -24,5 +25,33 @@ public class FileFilter {
      * @param extension 要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
+        List<String> filteredname = new ArrayList<>();
+        Files.walkFileTree(rootDirectory, new SimpleFileVisitor<Path>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.getFileName().toString().endsWith(extension)) {
+                    filteredname.add(file.getFileName().toString());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return filteredname;
+    }
 }
+
+/*    static class FileFilterVistor extends SimpleFileVisitor<Path> {
+        private String extension;
+        private List<String> filteredname = new ArrayList<>();
+
+        public FileFilterVistor(String extension) {
+            this.extension = extension;
+        }
+
+        public List<String> getFilteredname() {
+            return filteredname;
+        }
+
+
+    }*/
+
