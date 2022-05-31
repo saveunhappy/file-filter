e879043a5c93e91e680f9ca3f0c37a64379a0899
lei <106215839+3stonelei@users.noreply.github.com>
2022-05-31 12:02:54 +0800

    文件过滤器  匿名内部类 (#228)
    文件过滤器  匿名内部类 (#228)

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.polymorphism;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileFilter {
@@ -24,5 +26,19 @@ public class FileFilter {
      * @param extension 要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
+        List<String> fileName = new ArrayList<>();
+        Files.walkFileTree(rootDirectory, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.getFileName().toString().endsWith(extension)) {
+                    fileName.add(file.getFileName().toString());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+        return fileName;
+    }
+
 }
