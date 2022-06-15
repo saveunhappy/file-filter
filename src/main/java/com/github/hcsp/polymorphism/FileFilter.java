a7d304559374e80800621e9dc2897d91b4a3ac84
Ye Shiqing <908202572@qq.com>
2022-06-15 21:59:47 +0800

    实现一个自定义的文件过滤器 (#229)
    实现一个自定义的文件过滤器 (#229)

--- src/main/java/com/github/hcsp/polymorphism/FileFilter.java
+++ src/main/java/com/github/hcsp/polymorphism/FileFilter.java
@@ -1,8 +1,9 @@
 package com.github.hcsp.polymorphism;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileFilter {
@@ -21,8 +22,31 @@ public class FileFilter {
      * 实现一个按照扩展名过滤文件的功能
      *
      * @param rootDirectory 要过滤的文件夹
-     * @param extension 要过滤的文件扩展名，例如 .txt
+     * @param extension     要过滤的文件扩展名，例如 .txt
      * @return 所有该文件夹（及其后代子文件夹中）匹配指定扩展名的文件的名字
      */
-    public static List<String> filter(Path rootDirectory, String extension) throws IOException {}
+    public static List<String> filter(Path rootDirectory, String extension) throws IOException {
+        FileFilterVisitor visitor = new FileFilterVisitor(extension);
+        Files.walkFileTree(rootDirectory, visitor);
+        return visitor.getFilteredNames();
+    }
+
+    private static class FileFilterVisitor extends SimpleFileVisitor<Path> {
+        private final String extension;
+        private final List<String> filteredNames = new ArrayList<>();
+        public List<String> getFilteredNames() {
+            return filteredNames;
+        }
+        public FileFilterVisitor(String extension) {
+            this.extension = extension;
+        }
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            String fileName = file.getFileName().toString();
+            if (fileName.endsWith(extension)) {
+                filteredNames.add(fileName);
+            }
+            return FileVisitResult.CONTINUE;
+        }
+    }
 }
